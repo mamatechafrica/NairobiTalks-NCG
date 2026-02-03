23f53b7ac8fb40dadc03fec7f131ce0b78b411bc
nashthecoder <naijeria@gmail.com>
2026-02-03 07:55:14 +0300

    Fix Trix includes: pin via importmap and load CSS from jsDelivr

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "fireworks-js", to: "https://ga.jspm.io/npm:fireworks-js@2.10.0/dist/index.es.js"
+pin "trix", to: "https://cdn.jsdelivr.net/npm/trix@2.0.0/dist/trix.umd.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
